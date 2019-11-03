d1f30f2ccff7d635575fe5fea0eed3845798e908
Yuhan Liu <liuyuhanheidi@163.com>
2019-11-02 23:18:47 -0400

    Heidi - added docstring and main()

--- customer_analysis.py
+++ customer_analysis.py
@@ -6,6 +6,14 @@ import matplotlib.pyplot as plt
 
 
 def categorize_feature(f):
+    """ Categorize features according to first level description
+    Determine the category of each topic by taking the term before the second
+    '/' mark. Put the topic IDs belonging to the same category under the same
+    key in a dictionary.
+
+    :param f: DataFrame
+    :return: dictionary relating categories to topic ID
+    """
     all_topics = {}
     feature = f.to_numpy()
     for x in range(len(feature[:, 1])):
@@ -21,6 +29,15 @@ def categorize_feature(f):
 
 
 def label_customer(user_id, data, all_topics):
+    """ Label each customer with their most interested category
+    Determine the most interested topic of a certain user with the category
+    in which the user's interest has the hightest sum.
+
+    :param user_id: int containing user ID
+    :param data: DataFrame
+    :param all_topics: dictionary containing all categories
+    :return: int containing user ID and string containing label
+    """
     d = data.to_numpy()
     index = [i for i, x in enumerate(d[:, 0]) if x == user_id][0]
     interest = d[index, 2]  # dict
@@ -41,6 +58,15 @@ def label_customer(user_id, data, all_topics):
 
 
 def cluster_user(data, all_topics):
+    """ Cluster users according to label and find conversion rate
+    Put all user IDs of the customers sharing the same label. Use the ratio of
+    converted users over all users in a cluster for the conversion rate of the
+    category.
+
+    :param data:
+    :param all_topics:
+    :return:
+    """
     d = data.to_numpy()
     user_n = int(np.shape(d)[0])
     conversion = {}
@@ -65,10 +91,14 @@ def cluster_user(data, all_topics):
     return conversion_rate
 
 
-training = load("training.csv")
-interest_topics = pd.read_csv("interest_topics.csv")
+def main():
+    training = load("training.csv")
+    interest_topics = pd.read_csv("interest_topics.csv")
 
-all_t = categorize_feature(interest_topics)
-# [_, Label] = label_customer(5, training, all_t)
-conversion_rate = cluster_user(training, all_t)
-print(conversion_rate)
+    all_t = categorize_feature(interest_topics)
+    [_, Label] = label_customer(5, training, all_t)
+    conversion_rate = cluster_user(training, all_t)
+
+
+if __name__ == '__main__':
+    main()
